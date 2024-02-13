b474b45aca24d99e0c27a1b1cd292f47c40ff71c
purvadubal <159678886+purvadubal@users.noreply.github.com>
2024-02-13 15:33:31 +0530

    Add files via upload

---
+++ hiber1/src/main/java/com/sgm/hiber1/Place.java
@@ -0,0 +1,38 @@
+package com.sgm.hiber1;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="place")
+public class Place {
+@Id
+private int id;
+private String name;
+private int fare;
+public int getId() {
+return id;
+}
+public void setId(int id) {
+this.id = id;
+}
+public String getName() {
+return name;
+}
+public void setName(String name) {
+this.name = name;
+}
+public int getFare() {
+return fare;
+}
+public void setFare(int fare) {
+this.fare = fare;
+}
+@Override
+public String toString() {
+return "Place [id=" + id + ", name=" + name + ", fare=" + fare + "]";
+}
+
+
+}
